92789a2f11c6494094a68df808eb6719b2530e69
Julien Raynal <julien@Juliens-MacBook-Pro.local>
2026-07-06 11:26:36 +0200

    Custom formatter, lsp and color theme

--- init.lua
+++ init.lua
@@ -382,18 +382,18 @@ do
   -- change the command under that to load whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  vim.pack.add { gh 'folke/tokyonight.nvim' }
+  vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
   ---@diagnostic disable-next-line: missing-fields
-  require('tokyonight').setup {
+  require("catppuccin").setup {
     styles = {
-      comments = { italic = false }, -- Disable italics in comments
+      comments = { }, 
     },
   }
 
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
+  vim.cmd.colorscheme 'catppuccin-macchiato'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
@@ -702,6 +702,7 @@ do
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
     -- ts_ls = {},
+    ty = {},
 
     stylua = {}, -- Used to format Lua code
 
@@ -799,6 +800,7 @@ do
       -- rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
+      python = { "ruff", },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
